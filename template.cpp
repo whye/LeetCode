a66c6f24c3ce34df65a1ad14e155f673b63e96b9
System Administrator <root@10-8-234-35.vlan877.client.wireless.uic.edu>
2016-09-17 18:31:19 -0500

    template and 292

---
+++ template.cpp
@@ -0,0 +1,22 @@
+//
+//description:
+/*
+*/
+
+
+//information
+
+
+
+
+#include<iostream>
+
+using namespace std;
+
+
+int main(){
+        Solution solu;
+        cout << solu.;
+
+        return 0;
+}
